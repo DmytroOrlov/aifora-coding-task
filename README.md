8e6294a531bfa9f4b424288c66ae32ac3e48c785
Dmytro Orlov <orlov.dm@gmail.com>
2021-01-24 20:04:27 +0100

    2021-01-24-20-04-27

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## Math
+According to https://en.wikipedia.org/wiki/Poisson_distribution
+
+P(`at least one hurricane`) = 1-P(`no hurricane`) = 1-e^(-λ)
+
+where λ - Average
+
 ## Developing in Sbt
 ```sh
 $ sbt
--- src/main/scala/hurricane/enpoint/Endpoints.scala
+++ src/main/scala/hurricane/enpoint/Endpoints.scala
@@ -1,9 +1,9 @@
 package hurricane.enpoint
 
+case class MostHurricanes(year: Int, month: Int)
+
+case class HurricanePossibility(value: Double)
+
 object Endpoints {
-
-  case class MostHurricanes(year: Int, month: Int)
-
-  case class HurricanePossibility(value: Double)
 
 }
