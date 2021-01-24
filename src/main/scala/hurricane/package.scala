cecc0c5d24c7b1df128ea5854b5987c528ca6fc9
Dmytro Orlov <orlov.dm@gmail.com>
2021-01-24 22:54:28 +0100

    2021-01-24-22-54-28

--- src/main/scala/hurricane/package.scala
+++ src/main/scala/hurricane/package.scala
@@ -1,3 +1,5 @@
+import io.circe.Codec
+import io.circe.generic.extras.semiauto.deriveUnwrappedCodec
 import sttp.tapir.Codec.PlainCodec
 import sttp.tapir.generic.Configuration
 
@@ -5,8 +7,11 @@ package object hurricane {
   implicit val tapirSnakeCaseConfig =
     Configuration.default.withSnakeCaseMemberNames
 
-  implicit val yearCodec: PlainCodec[Year] =
+  implicit val `Codec[Month]`: Codec[Month] = deriveUnwrappedCodec
+  implicit val `Codec[Year]`: Codec[Year] = deriveUnwrappedCodec
+
+  implicit val `PlainCodec[Year]`: PlainCodec[Year] =
     implicitly[PlainCodec[Int]].map(Year)(_.value)
-  implicit val monthCodec: PlainCodec[Month] =
+  implicit val `PlainCodec[Month]`: PlainCodec[Month] =
     implicitly[PlainCodec[String]].map(Month)(_.value)
 }
